2c1b65a9ea903935fbd4727c99db929df4f0d6d2
manu <manuthala7@gmail.com>
2024-12-23 22:27:15 +0530

    First Module first commit

---
+++ 01_Module/01_variables.js
@@ -0,0 +1,11 @@
+var a = 20;
+console.log(a);
+
+var name = "manu";
+console.log(name);
+
+var greet = "Hello";
+console.log(greet);
+
+var isAvilable = true;
+console.log(isAvilable);
---
+++ 01_Module/script.js
@@ -0,0 +1,4 @@
+console.log("Hello from script file");
+
+let a = 77;
+console.log(a);
